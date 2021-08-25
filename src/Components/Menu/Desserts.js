26eeddb947fa259c6d8ad7c8e86ba94bdb3e3689
renaatosalgado <renaato.salgado@gmail.com>
2021-08-25 09:11:49 -0300

    added desserts props

--- src/Components/Menu/Desserts.js
+++ src/Components/Menu/Desserts.js
@@ -1,47 +1,51 @@
+const desserts = [
+  {
+    img: "../../../public/img/pudim.png",
+    name: "Pudim",
+    description: "Pedaço",
+    price: "R$ 4,90",
+  },
+  {
+    img: "../../../public/img/pudim.png",
+    name: "Torta Holandesa",
+    description: "Pedaço",
+    price: "R$ 9,90",
+  },
+  {
+    img: "../../../public/img/pudim.png",
+    name: "Bomba de chocolate",
+    description: "Unidade",
+    price: "R$ 7,50",
+  },
+];
+
+const Dessert = (props) => {
+  return (
+    <div class="opcao" onclick="selecionarSobremesa(this, 'Pudim', 7.90)">
+      <img src={props.img} alt="" />
+      <div class="titulo">{props.name}</div>
+      <div class="descricao">{props.description}</div>
+      <div class="fundo">
+        <div class="preco">{props.price}</div>
+        <div class="check">
+          <ion-icon name="checkmark-circle"></ion-icon>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Desserts() {
   return (
     <>
       <div class="secao">
         <div class="titulo">Por fim, sua sobremesa</div>
-        <div class="opcoes sobremesa">
-          <div class="opcao" onclick="selecionarSobremesa(this, 'Pudim', 7.90)">
-            <img src="img/pudim.png" alt="" />
-            <div class="titulo">Pudim</div>
-            <div class="descricao">Lata 350ml</div>
-            <div class="fundo">
-              <div class="preco">R$ 7,90</div>
-              <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-              </div>
-            </div>
-          </div>
-
-          <div class="opcao" onclick="selecionarSobremesa(this, 'Flam', 7.90)">
-            <img src="img/pudim.png" alt="" />
-            <div class="titulo">Flam</div>
-            <div class="descricao">Lata 350ml</div>
-            <div class="fundo">
-              <div class="preco">R$ 7,90</div>
-              <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-              </div>
-            </div>
-          </div>
-
-          <div
-            class="opcao"
-            onclick="selecionarSobremesa(this, 'Brigadeiro', 7.90)">
-            <img src="img/pudim.png" alt="" />
-            <div class="titulo">Brigadeiro</div>
-            <div class="descricao">Lata 350ml</div>
-            <div class="fundo">
-              <div class="preco">R$ 7,90</div>
-              <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div class="opcoes sobremesa"></div>
+        
+        {desserts.map((dessert, index) => 
+        <Dessert img={dessert.img} name={dessert.name} description={dessert.description} price={dessert.price} key={index} />
+        )};
+        
       </div>
     </>
   );
